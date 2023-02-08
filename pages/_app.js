e2abe8e6e45c7c9b800f92f3b36e68edc1901987
ManuelSdev <masanchezzm@gmail.com>
2023-02-08 17:43:04 +0100

    -add nextjs modularize imports and --install @emotion/server to fix _document.js/getInitialProps for SSR CSS without FOUC

--- pages/_app.js
+++ pages/_app.js
@@ -15,11 +15,11 @@ export default function MyApp(props) {
 
   return (
     <CacheProvider value={emotionCache}>
-      {/**
-        <Head>
+
+      <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-       */}
+
 
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
