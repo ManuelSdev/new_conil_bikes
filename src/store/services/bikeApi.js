93b690340e4a2ccf6f20df536b3d7a5b76530a6f
ManuelSdev <masanchezzm@gmail.com>
2023-02-09 13:33:51 +0100

    -install prettier and config slint for prettier

--- src/store/services/bikeApi.js
+++ src/store/services/bikeApi.js
@@ -1,41 +1,54 @@
-import {baseApi} from "./baseApi";
-const urlParams = (obj) => new URLSearchParams(obj);
+import { baseApi } from './baseApi'
+const urlParams = (obj) => new URLSearchParams(obj)
 
 const bikeApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getBikes: builder.query({
       query: (filters) => `/bikes?${filters}`,
     }),
+
     getBikesById: builder.query({
       query: (arrayOfIds) => ({
-        url: "/bikes",
-        method: "POST",
+        url: '/bikes',
+        method: 'POST',
 
         body: arrayOfIds,
       }),
     }),
 
     getAvaiableSizes: builder.query({
-      query: ({from, to}) => `/bikes/avaiable-sizes?${urlParams({from, to})}`,
+      query: ({ from, to }) =>
+        `/bikes/avaiable-sizes?${urlParams({ from, to })}`,
     }),
     getAvaiableTypes: builder.query({
-      query: ({from, to, size}) =>
-        `/bikes/avaiable-types?${urlParams({from, to, size})}`,
+      query: ({ from, to, size }) =>
+        `/bikes/avaiable-types?${urlParams({ from, to, size })}`,
     }),
     getAvaiableRanges: builder.query({
-      query: ({from, to, size, type}) =>
-        `/bikes/avaiable-ranges?${urlParams({from, to, size, type})}`,
+      query: ({ from, to, size, type }) =>
+        `/bikes/avaiable-ranges?${urlParams({
+          from,
+          to,
+          size,
+          type,
+        })}`,
     }),
     getAvaiableBikes: builder.query({
-      query: ({from, to, size, type, range}) =>
-        `/bikes/avaiable?${urlParams({from, to, size, type, range})}`,
+      query: ({ from, to, size, type, range }) =>
+        `/bikes/avaiable?${urlParams({
+          from,
+          to,
+          size,
+          type,
+          range,
+        })}`,
     }),
   }),
-});
+})
 //const [trigger, result, lastPromiseInfo] = baseApi.endpoints.getSizes.useLazyQuery()
 
 export const useGetAvaiableBikesQueryState =
-  bikeApi.endpoints.getAvaiableBikes.useQueryState;
+  bikeApi.endpoints.getAvaiableBikes.useQueryState
 
 export const {
   useGetBikesQuery,
@@ -47,4 +60,4 @@ export const {
   useGetAvaiableRangesQuery,
   useGetAvaiableBikesQuery,
   useLazyGetAvaiableBikesQuery,
-} = bikeApi;
+} = bikeApi
