b497eb7ee4f037e9b67998e64efed6d26971a145
ManuelSdev <masanchezzm@gmail.com>
2023-03-08 09:19:25 +0100

    uninstall tailwindCSS and running full mui

--- src/components/elements/HomeSectionCard.js
+++ src/components/elements/HomeSectionCard.js
@@ -14,7 +14,18 @@ export const HomeSectionCard = ({
 }) => {
    return (
       <Paper
-         sx={{ backgroundColor: 'dark.c' }}
+         sx={{ backgroundColor: 'dark.c',
+         position:'relative',
+         display:'flex',
+         flexDirection:'column',
+         width:'100%',
+         alignItems:'center',
+         justifyContent:'flex-start',
+         overflow:'hidden',
+         borderRadius:'50px',
+         paddingBottom:4,
+
+       }}
          elevation={1}
          //      variant="outlined"
          className="relative flex
@@ -22,7 +33,11 @@ export const HomeSectionCard = ({
       >
          <Box
             //className="relative flex h-screen  justify-items-start overflow-hidden rounded-t-[50px]"
-            //  sx={{ height: '100px' }}
+            sx={{ height: '200px' ,
+         position:'relative',
+         width:'100%',
+         overflow:'hidden',
+         }}
             //CLAVE: Controla width aquí
             className="relative h-[200px] w-full overflow-hidden"
          >
@@ -32,14 +47,40 @@ export const HomeSectionCard = ({
                src={src}
                width="300"
                height="200"
+               style={{
+                  zIndex:0,
+                  height:'250px',
+                  width:'100%',
+               }}
                // fill
             />
-            <Typography className="absolute bottom-2  left-2 z-30 w-fit border-solid border-white bg-black/[0.4] p-3 text-white ">
+            <Typography 
+            sx={{
+               position:'absolute',
+               bottom:10,
+               left:10,
+               zIndex:30,
+               width:'fit-content',
+               borderStyle:'solid',
+               borderColor:'white',
+               bgcolor:'rgba(0, 0, 0, 0.4)',
+               padding:1,
+               color:'white',
+               
+
+            }}
+            className="absolute bottom-2  left-2 z-30 w-fit border-solid border-white bg-black/[0.4] p-3 text-white ">
                {children}
             </Typography>
          </Box>
 
-         <Typography className="p-4 text-justify text-[#BCBCBC]">
+         <Typography 
+         sx={{
+            padding:2,
+            textAlign:'justify',
+            color:'#BCBCBC'
+         }}
+         className="p-4 text-justify text-[#BCBCBC]">
             {text}
          </Typography>
          <Divider sx={{ pb: 2 }} />
